719e1f5f186407f77305185c5e8f69310672689f
gadam7 <g.adamidis1985@gmail.com>
2023-05-29 23:21:41 +0300

    CF23-v0.1

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Είναι τεκμηρίωση, εκτυπώνει Hello World! στο
+ * standard output (κονσόλα).
+ *
+ * @author adminmacbook
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+       // Prints Hello World
+
+       /*
+        *
+        */
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/gr/aueb/cf/testbed/ch1/InitialsApp.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.testbed.ch1;
+
+public class InitialsApp {
+    public static void main(String[] args) {
+
+        System.out.println(" ****        *");
+        System.out.println("*    *      *  *");
+        System.out.println("*          *    *");
+        System.out.println("*   ***   ********");
+        System.out.println("*    *   *        *");
+        System.out.println(" ****   *          *");
+    }
+}
